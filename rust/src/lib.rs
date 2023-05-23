99a7ecac129b3abd6cffd82ec41d3f4c4db8c6f8
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-05-23 21:26:11 +0600

    rust code in lib.rs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+#[no_mangle] //? Derivative attribute
+
+//? pub extern "C" is used to make the function available to C
+pub extern "C" fn print_something() {
+    println!("Print Rust Function")
 }
