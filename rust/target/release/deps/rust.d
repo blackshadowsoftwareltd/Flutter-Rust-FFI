ea71ab4cb2352cc6cab0aa3ad252ae5955cab4c1
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-05-23 21:29:33 +0600

    generated some files. .so file for linux

---
+++ rust/target/release/deps/rust.d
@@ -0,0 +1,5 @@
+/home/remon/Flutter/flutter_rust_ffi/rust/target/release/deps/librust.so: src/lib.rs
+
+/home/remon/Flutter/flutter_rust_ffi/rust/target/release/deps/rust.d: src/lib.rs
+
+src/lib.rs:
---
+++ rust/target/release/librust.d
@@ -0,0 +1,1 @@
+/home/remon/Flutter/flutter_rust_ffi/rust/target/release/librust.so: /home/remon/Flutter/flutter_rust_ffi/rust/src/lib.rs
