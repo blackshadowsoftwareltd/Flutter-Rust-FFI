006ec16fad3fc80f4e73f8d2f08e94ed3ef74669
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-10-09 13:06:37 +0600

    sled read write

--- lib/src/ffi/db/sled.dart
+++ lib/src/ffi/db/sled.dart
@@ -5,24 +5,20 @@ import 'package:ffi/ffi.dart';
 import 'package:flutter_rust_ffi/src/ffi/ffi.dart';
 import 'package:path_provider/path_provider.dart';
 
-typedef OpenDbFunc = Pointer<Void> Function(Pointer<Uint8>);
-typedef OpenDbFuncNative = Pointer<Void> Function(Pointer<Uint8>);
+typedef OpenDbRust = Void Function(Pointer<Uint8> path);
+typedef OpenDbDart = void Function(Pointer<Uint8>);
 
-final OpenDbFunc _openDb = dynamicLib
-    .lookup<NativeFunction<OpenDbFuncNative>>('open_db')
-    .asFunction<OpenDbFunc>();
-
-late Pointer<Void> db;
+final void Function(Pointer<Uint8>) openDbLookup =
+    dynamicLib.lookupFunction<OpenDbRust, OpenDbDart>('open_db');
 
 // Open a sled db with a given path
 Future<void> openDb() async {
   try {
     final p = await getApplicationDocumentsDirectory();
     final path = p.path.toNativeUtf8();
-    db = _openDb(path.cast());
+    openDbLookup(path.cast());
   } catch (e) {
     log(e.toString());
   }
   await Future.delayed(const Duration(seconds: 2));
-  log(db.runtimeType.toString());
 }
--- rust/src/db.rs
+++ rust/src/db.rs
@@ -1,11 +1,55 @@
 use std::{ffi::CStr, os::raw::c_char};
 
-use sled::Db;
+use sled::{Db, IVec};
 
 #[no_mangle]
-pub extern "C" fn open_db(path: *const c_char) -> *mut Db {
+pub extern "C" fn open_db(path: *const c_char) {
     println!("Opening DB");
     let path_str = unsafe { CStr::from_ptr(path).to_str().unwrap() };
     let db = sled::open(path_str).unwrap();
-    Box::into_raw(Box::new(db))
+    do_something_with_db(&db);
+    // Box::into_raw(Box::new(db))
+    db.flush().unwrap();
+}
+
+fn do_something_with_db(db: &Db) {
+    let time = std::time::SystemTime::now();
+
+    match db.get("key") {
+        Ok(v) => match v {
+            Some(v) => {
+                println!("Previous : {:?}", ivect_to_string(v).unwrap())
+            }
+            None => {
+                println!("Previous : None")
+            }
+        },
+        Err(e) => {
+            println!("Error : {}", e)
+        }
+    }
+    let time = format!("Time {:?}", time);
+
+    db.insert("key", time.as_bytes()).unwrap();
+
+    match db.get("key") {
+        Ok(v) => match v {
+            Some(v) => {
+                println!("New : {:?}", ivect_to_string(v).unwrap())
+            }
+            None => {
+                println!("New : None")
+            }
+        },
+        Err(e) => {
+            println!("Error : {}", e)
+        }
+    }
+}
+
+fn ivect_to_string(iv: IVec) -> Result<String, std::str::Utf8Error> {
+    let bytes: &[u8] = iv.as_ref();
+    let string = std::str::from_utf8(bytes)?;
+
+    Ok(string.to_string())
 }
